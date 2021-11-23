cae68a04b67ecaff5ec67189ab630f0a897eeafd
Caro <CA252755@fi365.ort.edu.uy>
2021-11-23 20:45:23 -0300

    d

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ $DB_HOST = 'localhost';
 $DB_USER = 'obl';
 $DB_PASSWORD = 'obli1234';
 $DB_DATABASE = 'idukan';
-$PORT = 3306
+$PORT = 3306;
 
 
 $link = new mysqli($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_DATABASE,$PORT);
