fc5d4a4ac1ae4f74cc45c56f318bfc28ac81b505
Caro <CA252755@fi365.ort.edu.uy>
2021-11-23 20:28:37 -0300

    s

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
-$$DB_HOST = 'localhost';
-$$DB_USER = 'root';
-$$DB_PASSWORD = 'root';
-$$DB_DATABASE = 'idukan';
+$DB_HOST = 'localhost';
+$DB_USER = 'root';
+$DB_PASSWORD = 'root';
+$DB_DATABASE = 'idukan';
 
 
-$link = new mysqli($$DB_HOST,$$DB_USER,$$DB_PASSWORD,$$DB_DATABASE);
+$link = new mysqli($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_DATABASE);
 
 // Check connection
-if ($mysqli -> connect_errno) {
+if ($link -> connect_errno) {
   echo "Failed to connect to MySQL: " . $link-> connect_error;
   exit();
 }
